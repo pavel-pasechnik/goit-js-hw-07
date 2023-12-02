8237fdae917ceb856fe8e7e610b4e6ac797326e7
Pavel <Pavel.Pasechnik@gmail.com>
2023-12-02 14:48:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# goit-js-hw-06
+
+Homework 6
---
+++ js/task-3.js
@@ -0,0 +1,34 @@
+"use strict";
+
+class StringBuilder {
+  #value = "";
+
+  constructor(value) {
+    this.#value = value;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    return (this.#value += str);
+  }
+
+  padStart(str) {
+    return (this.#value = str + this.#value);
+  }
+
+  padBoth(str) {
+    return (this.#value = str + this.#value + str);
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
